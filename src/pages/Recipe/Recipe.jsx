c9ae9d18b2e495a6a186343d78896eabf11dc5e9
Humberto Peniche Batista <humberto.penichebatista@dpird.wa.gov.au>
2023-04-03 15:48:33 +0800

    Recipe page cleanup

--- src/pages/Recipe/Recipe.jsx
+++ src/pages/Recipe/Recipe.jsx
@@ -1,67 +1,30 @@
 import Loading from "../../components/Helpers/Loading";
 import { useParams, useLocation } from "react-router-dom";
-import { Box, Card, CardMedia, CardContent, Container, Divider, Rating, Typography } from "@mui/material";
-import ScheduleIcon from "@mui/icons-material/Schedule";
-import StarIcon from "@mui/icons-material/Star";
-import { width } from "@mui/system";
+import { Container, List, ListItem, Typography } from "@mui/material";
+
 const Recipe = () => {
   const recipeData = useLocation().state; //From state
   console.log(recipeData)
   const id = useParams().id;
+  console.log(id)
+
   if (recipeData) {
+    let instructions = recipeData.instructions.replace(/<ol>|<\/ol>|<li>|<\/li>/g, "\n");
+    instructions = instructions.split("\n").filter((instruction) => instruction !== "");
     return (
       <Container sx={{backgroundColor: "#f7f7f7"}}>
-        <Typography variant="h4" align="center">
-          {recipeData.title}
-        </Typography>
-        <Card
-          sx={{
-            height: "100%",
-            display: "flex",
-            flexDirection: "column",
-            userSelect: "none",
-            ":hover": {
-              boxShadow: 5,
-              cursor: "pointer"
-            },
-          }}
-        >
-          <CardMedia
-            component="img"
-            sx={{
-              // 16:9
-              p: 0,
-              height: 200,
-              width: 200
-            }}
-            image={recipeData.image}
-            alt="food-image"
-          />
-          <CardContent sx={{ flexGrow: 1 }}>
-            <Typography gutterBottom variant="h6" component="h3" sx={{ height: 64, lineHeight: 1.25 }}>
-              {recipeData.title}
-            </Typography>
-            <Divider sx={{mt: 1}} orientation="horizontal" />
-            <Box sx={{ display: "flex", alignItems: "center", mt: 2.5, height: 5 }}>
-              <ScheduleIcon sx={{mr: 1}} fontSize="extra-small" />
-              <Typography color="grey">{recipeData.readyInMinutes ? recipeData.readyInMinutes : "0"} mins</Typography>
-              <Rating
-                sx={{ ml: "auto" }}
-                defaultValue={
-                  Math.floor((Math.random() * 5) / 0.5) * 0.5 + 3
-                }
-                emptyIcon={<StarIcon style={{ opacity: 0.75 }} fontSize="inherit" />}
-                precision={0.5}
-                readOnly
-              />
-            </Box>
-          </CardContent>
-        </Card>
+        {recipeData.extendedIngredients.map((ingredient, index) => (
+          <Typography key={index}>{ingredient.original}</Typography>
+        ))}
+        <List>
+          {instructions.map((instruction, index) => (
+            <ListItem key={index}>{`${index + 1} - ${instruction}`}</ListItem>
+          ))}
+        </List>
       </Container>
     )
-  }
-  else {
-    <Loading/>
+  } else {
+    return <Loading/>;
   }
 }
 
